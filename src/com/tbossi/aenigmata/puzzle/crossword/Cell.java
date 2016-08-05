4cccf28430401a77e59624e85560a958a8c8e848
Tommaso Bossi <tommasobossi95@gmail.com>
2016-08-05 17:14:45 +0200

    minor fix

--- src/com/tbossi/aenigmata/puzzle/crossword/Cell.java
+++ src/com/tbossi/aenigmata/puzzle/crossword/Cell.java
@@ -1,5 +1,59 @@
-package aenigmata.puzzle.crossword;
+package com.tbossi.aenigmata.puzzle.crossword;
 
-public abstract class Cell {
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
+public abstract class Cell<K, C> {
+	
+	private ObjectProperty<K> key;
+	private ObjectProperty<C> content;
+	private SimpleBooleanProperty cellVisible;
+	
+	public Cell() {
+		key = new SimpleObjectProperty<K>();
+		content = new SimpleObjectProperty<C>();
+		cellVisible = new SimpleBooleanProperty();
+	}
+	
+	public ObjectProperty<K> keyProperty() {
+		return key;
+	}
+	
+	public K getKey() {
+		return key.get();
+	}
+	
+	public void setKey(K key) {
+		this.key.set(key);
+	}
+	
+	
+	public ObjectProperty<C> contentProperty() {
+		return content;
+	}
+	
+	public C getContent() {
+		return content.get();
+	}
+	
+	public void setContent(C content) {
+		this.content.set(content);
+	}
+	
+	public SimpleBooleanProperty cellVisibleProperty() {
+		return cellVisible;
+	}
+	/**
+	 * @return the cellVisible
+	 */
+	public boolean getCellVisible() {
+		return cellVisible.get();
+	}
+	/**
+	 * @param cellVisible the cellVisible to set
+	 */
+	public void setCellVisible(boolean cellVisible) {
+		this.cellVisible.set(cellVisible);
+	}
 }
